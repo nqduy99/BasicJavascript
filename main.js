23bcbb7880de99c705ebf8631a62d1dd70fc3337
Nguyễn Quang Duy <nqduy98999@gmail.com>
2022-02-16 20:54:46 +0700

    Finished Lam viec voi ham

--- main.js
+++ main.js
@@ -1,16 +1,53 @@
 
-var myString = 'Hoc Javascript tai F8!';
-var languages = 'Javascript, PHP, Ruby';
-var number = 345;
+var languages = ['Javascript', 'PHP', 'Ruby'];
+var language = 'Javascript JS Ruby';
 
-function run(title) {
-    var language = title.slice(4,14)
-    var rest = title.slice(14)
-    return `Language is ${language} and rest is ${rest}`;
+function getLastElementOfArray(input) {
+
+    var inputLength = input.length;
+    result = input[inputLength-1]
+
+    return result;
 }
 
-function run1(content) {
-    return content.replace(/JS/g, 'Javascript')
+function showDialog() {
+    alert('Hi xin chao Duy dep trai')
 }
 
-console.log (parseInt(number));
+function writeLog() {
+    var myString = '';
+    for (var param of arguments){
+        myString += `${param} -`
+        
+    }
+    console.log(myString)
+}
+
+function sum(a, b){
+    if (typeof(a) == 'number' && typeof(b) == 'number'){
+        return a+b;
+    }
+
+    return false;
+}
+
+function stringInString(needle , haystack ){
+    if(typeof needle =='string' && typeof haystack =='haystack' && haystack.indexOf(needle)>=0){
+        return true;
+    }
+    return false;
+}
+
+var emailKey = 'email';
+
+var myInfo = {
+    name: 'Duy Nguyen',
+    age: 18,
+    address: 'HCM, VN',
+    [emailKey]: 'nqduy@gmail.com'
+
+}
+
+var myKey = 'address'
+
+console.log(myInfo)
