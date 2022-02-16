e58e61aee6a6c8fbbe9f7d1dd7079530935edfb8
Nguyễn Quang Duy <nqduy98999@gmail.com>
2022-02-16 21:35:37 +0700

    Finished Part 8 Lam viec voi Object

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
-function User(firstName, lastName, avatar){
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this. avatar = avatar;
-    this.getName = function(){
-        return `${this.firstName} ${this.lastName}`;
-    }
-}
 
-User.prototype.className = 'F8'
+var date = new Date()
 
-var user = new User('Duy', 'Nguyen', 'avatar');
-var user2 = new User('Son', 'Dang', 'avatar');
-console.log(user)
+var year = date.getFullYear()
+
+var month = date.getMonth() + 1;
+
+var day = date.getDate()
+
+console.log(date)
