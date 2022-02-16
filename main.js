5b68f3c320c282a65c14315be533912fbe20463b
Nguyễn Quang Duy <nqduy98999@gmail.com>
2022-02-16 21:06:43 +0700

    Finished Object Prototype

--- main.js
+++ main.js
@@ -1,53 +1,14 @@
-
-var languages = ['Javascript', 'PHP', 'Ruby'];
-var language = 'Javascript JS Ruby';
-
-function getLastElementOfArray(input) {
-
-    var inputLength = input.length;
-    result = input[inputLength-1]
-
-    return result;
-}
-
-function showDialog() {
-    alert('Hi xin chao Duy dep trai')
-}
-
-function writeLog() {
-    var myString = '';
-    for (var param of arguments){
-        myString += `${param} -`
-        
+function User(firstName, lastName, avatar){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this. avatar = avatar;
+    this.getName = function(){
+        return `${this.firstName} ${this.lastName}`;
     }
-    console.log(myString)
 }
 
-function sum(a, b){
-    if (typeof(a) == 'number' && typeof(b) == 'number'){
-        return a+b;
-    }
+User.prototype.className = 'F8'
 
-    return false;
-}
-
-function stringInString(needle , haystack ){
-    if(typeof needle =='string' && typeof haystack =='haystack' && haystack.indexOf(needle)>=0){
-        return true;
-    }
-    return false;
-}
-
-var emailKey = 'email';
-
-var myInfo = {
-    name: 'Duy Nguyen',
-    age: 18,
-    address: 'HCM, VN',
-    [emailKey]: 'nqduy@gmail.com'
-
-}
-
-var myKey = 'address'
-
-console.log(myInfo)
+var user = new User('Duy', 'Nguyen', 'avatar');
+var user2 = new User('Son', 'Dang', 'avatar');
+console.log(user)
