e02082dcc016e14b917b186f245ad478e45fc601
Nguyễn Quang Duy <nqduy98999@gmail.com>
2022-02-18 16:15:40 +0700

    Finished Part 12 Callback

--- main.js
+++ main.js
@@ -6,24 +6,89 @@ var courses = [
     {
         id: 1,
         name: 'Javascript',
-        coin: 0
+        coin: 100, 
+        isFinish: true
     },
     {
         id: 2,
         name: 'HTML, CSS',
-        coin: 1
+        coin: 1, 
+        isFinish: true
     },
     {
         id: 3,
         name: 'Ruby',
-        coin: 1
+        coin: 1, 
+        isFinish: true
     },
     {
         id: 4,
         name: 'PHP',
-        coin: 200
+        coin: 200, 
+        isFinish: false
     },
 ]
+
+
+// for (var index in courses){
+//     console.log(courses[index])
+// }
+
+// Array.prototype.some2 = function(callback){
+//     for(var index in this){
+//         if(this.hasOwnProperty(index)){
+//             var result = callback(this[index], index, this);
+//             if(result){
+//                 return true;
+//             }
+//         }
+//     }
+//     return false;
+// }
+
+// var result = courses.some2(function(course){
+//     return course.isFinish == true;
+// })
+
+// Array.prototype.every2 = function(callback){
+//     var output = true;
+
+//     for(var index in this){
+//         if(this.hasOwnProperty(index)){
+//             var result = callback(this[index], index, this);
+//             if(!result){
+//                 output = false;
+//                 break;
+//             }
+//         }
+//     }
+//     return output;
+// }
+
+// var result = courses.every2(function(course){
+//     return course.coin > 0;
+// })
+
+// console.log(result);
+
+// Array.prototype.filter2 = function(callback){
+//     var output = [];
+//     for(var index in this){
+//         if(this.hasOwnProperty(index)){
+//             var result = callback(this[index], index, this);
+//             if(result){
+//                 output.push(this[index]);
+//             }
+//         }
+//     }
+//     return output;
+// }
+
+// var filterCourses = courses.filter2(function(course, index){
+//     return course.coin > 0 ;
+// })
+
+//console.log(filterCourses);
 
 function courseHandler(course, index) {
     return {
@@ -63,5 +128,38 @@ function coinHandler(accumulator, currentValue, currentIndex, originArray){
 var totalCoin = courses.reduce(coinHandler, 0)
 // console.log(totalCoin)
 
+//Ham reduce2()
+const numbers = [1, 2, 3, 4, 5]
 
+//console.log(typeof numbers)
+
+// Array.prototype.reduce2 = function(callback, result){
+//     let i = 0;
+//     if(arguments.length < 2){
+//         i = 1;
+//         result = this[0];
+//     }
+//     for(; i < this.length; i++){
+//         result = callback(result, this[i], i, this)
+//     }
+//     return result;
+// }
+
+// var result = numbers.reduce2(function(acc, number){
+//     return acc + number;
+// })
+
+//console.log(result)
+
+// Array.prototype.forEach2 = function(callback){
+//     for (var index in this){
+//         if(this.hasOwnProperty(index)){
+//             callback(this[index], index, this);
+//         }
+//     }
+// }
+
+// numbers.forEach2(function(number, index, array){
+//     console.log(number)
+// });
 
